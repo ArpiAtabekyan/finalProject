149b70f0756d741016b9770fc46e7bcd2ccf88a5
ArpiAtabekyan <arpi.atabekyan.y@tumo.org>
2021-09-26 12:51:26 +0400

    add file

---
+++ modules/GrassEater.js
@@ -0,0 +1,96 @@
+var LiveForm = require("./LiveForm");
+var random = require("./random.js");
+
+
+
+module.exports = class GrassEater extends LiveForm {
+    constructor(x, y, index) {
+        super(x, y, index);
+        this.life = 8;
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewCoordinates();
+        return super.chooseCell(character);
+    } 
+    mul() {
+        let emptyCells = this.chooseCell(0);
+        let newCell = random(emptyCells);
+
+        if (newCell && this.life >= 12) {
+            let x = newCell[0];
+            let y = newCell[1];
+            matrix[y][x] = this.index;
+            let grassEater = new GrassEater(x, y, this.index);
+            grassEaterArr.push(grassEater);
+            this.life = 5;
+        }
+    }
+    eat() {
+        let emptyCells = this.chooseCell(1);
+        let newCell = random(emptyCells);
+
+        if (newCell) {
+
+            this.life++;
+            let x = newCell[0];
+            let y = newCell[1];
+
+            matrix[y][x] = this.index;
+            matrix[this.y][this.x] = 0;
+
+            for (let i in grassArr) {
+                if (grassArr[i].x == x && grassArr[i].y == y) {
+                    grassArr.splice(i, 1)
+                    break;
+                }
+            }
+            this.x = x;
+            this.y = y;
+
+            if (this.life >= 13) {
+                this.mul();
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+    move() {
+        this.life--;
+        let emptyCells = this.chooseCell(0);
+        let newCell = random(emptyCells);
+
+        if (newCell) {
+            let x = newCell[0];
+            let y = newCell[1];
+            matrix[y][x] = 2;
+            matrix[this.y][this.x] = 0;
+            this.y = y;
+            this.x = x;
+        }
+        if (this.life < 0) {
+            this.die();
+        }
+    }
+    die() {
+        matrix[this.y][this.x] = 0;
+
+        for (let i in grassEaterArr) {
+            if (grassEaterArr[i].x == this.x && grassEaterArr[i].y == this.y) {
+                grassEaterArr.splice(i, 1)
+            }
+        }
+    }
+}
---
+++ server.js
@@ -0,0 +1,136 @@
+
+//! Requiring modules  --  START
+var Grass = require("./modules/Grass.js");
+var GrassEater = require("./modules/GrassEater.js");
+let random = require('./modules/random');
+//! Requiring modules  --  END
+
+
+//! Setting global arrays  --  START
+grassArr = [];
+grassEaterArr = [];
+let predatoryArr = [];
+let predatoryEaterArr = [];
+let randomCharacterEaterArr = [];
+
+matrix = [];
+grassHashiv = 0;
+//! Setting global arrays  -- END
+
+
+
+
+//! Creating MATRIX -- START
+function matrixGenerator(matrixSize, grass, grassEater, predatory, predatoryEater, randomCharacterEater) {
+    for (let i = 0; i < matrixSize; i++) {
+        matrix[i] = [];
+        for (let o = 0; o < matrixSize; o++) {
+            matrix[i][o] = 0;
+        }
+    }
+    for (let i = 0; i < grass; i++) {
+        let customX = Math.floor(random(matrixSize)); // 0-9
+        let customY = Math.floor(random(matrixSize)); // 4
+        matrix[customY][customX] = 1;
+    }
+    for (let i = 0; i < grassEater; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 2;
+    }
+    for (let i = 0; i < predatory; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 3;
+    }
+    for (let i = 0; i < predatoryEater; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 4;
+    }
+    for (let i = 0; i < randomCharacterEater; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 5;
+    }
+}
+matrixGenerator(20, 6, 4);
+//! Creating MATRIX -- END
+
+
+
+//! SERVER STUFF  --  START
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.use(express.static("."));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+server.listen(3000);
+//! SERVER STUFF END  --  END
+
+for (let i in grassArr) {
+    grassArr[i].mul();
+}
+
+for (let i in grassEaterArr) {
+    grassEaterArr[i].eat()
+}
+
+for (let i in predatoryArr) {
+    predatoryArr[i].eat()
+}
+
+for (let i in predatoryEaterArr) {
+    predatoryEaterArr[i].eat()
+}
+
+for (let i in randomCharacterEaterArr) {
+    randomCharacterEaterArr[i].eat()
+}
+
+
+
+function creatingObjects() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 2) {
+                var grassEater = new GrassEater(x, y);
+                grassEaterArr.push(grassEater);
+            } else if (matrix[y][x] == 1) {
+                var grass = new Grass(x, y);
+                grassArr.push(grass);
+                grassHashiv++;
+            }
+        }
+    }
+}
+creatingObjects();
+
+function game() {
+    if (grassArr[0] !== undefined) {
+        for (var i in grassArr) {
+            grassArr[i].mul();
+        }
+    }
+    if (grassEaterArr[0] !== undefined) {
+        for (var i in grassEaterArr) {
+            grassEaterArr[i].eat();
+        }
+    }
+
+    //! Object to send
+    let sendData = {
+        matrix: matrix,
+        grassCounter: grassHashiv
+    }
+
+    //! Send data over the socket to clients who listens "data"
+    io.sockets.emit("data", sendData);
+}
+
+
+
+setInterval(game, 1000)
